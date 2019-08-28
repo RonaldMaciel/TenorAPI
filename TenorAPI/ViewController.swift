64632bb34ae112674338ee1eaa29620c9a11161e
Matheus Lima Ferreira <matheuslf.17@gmail.com>
2019-08-28 13:56:43 -0300

    load images into TableView

--- TenorAPI/ViewController.swift
+++ TenorAPI/ViewController.swift
@@ -10,23 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     let request = Request()
-    var gifs : [Any] = []
+    var gifs : [URLGif] = []
     
     
     let dispatchGroup = DispatchGroup()
     
+    @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        dispatchGroup.notify(queue: .main) {
-            //quando completar
-        }
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         
         
-        DispatchQueue.global().async {
+        
+        
+        DispatchQueue.global(qos: .unspecified).async {
             
             self.dispatchGroup.enter()
             
@@ -34,14 +35,16 @@ class ViewController: UIViewController {
             self.loadGif {
                 print("terminou")
                 self.dispatchGroup.leave()
+            }
+            
+            self.dispatchGroup.notify(queue: .main) {
+                //quando completar
                 
+                print("acabou tudo")
+                self.tableView.reloadData()
             }
         }
-        
-        
     }
-    
-    
     
     
     func loadGif(callback: @escaping () -> Void) {
@@ -52,6 +55,52 @@ class ViewController: UIViewController {
     }
     
     
+}
+
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gifs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? TableViewCell {
+            
+            let gif_url = gifs[indexPath.row].url
+            cell.gif.load(url: URL(string: gif_url)!)
+            
+            
+            return cell
+        }
+        
+        return UITableViewCell()
+        
+        
+    }
+    
+    
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func loadGif(url: String){
+        DispatchQueue.global().async { [weak self] in
+            
+        }
+    }
 }
 
 
